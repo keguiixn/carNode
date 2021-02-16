b2517e8cbffd5bb091d1c1d052ae8ccb558ad8cf
guixin.ke <1258056704@qq.com>
2021-02-16 23:51:10 +0800

    '更新代码'

--- route.js
+++ route.js
@@ -68,6 +68,66 @@ router.post('/register', function(req, res) {
     })
 })
 
+// 获取全部用户信息
+router.get('/alluserInfo',(req,res)=>{
+    const sqlStr = `select * from user `
+    if(JSON.stringify(req.query)!=='{}'){
+        const {username} = req.query
+        const sql = `select * from user where username='${username}'`
+        conn.query(sql,(err,result)=>{
+            if(err){
+                return res.json({status:500,message:'获取信息失败'})
+            }
+            else{
+                return res.json({status:200,userInfo:result,total:result.length,page:20,pageSize:10})
+            }
+        })
+    }
+    else{
+        conn.query(sqlStr,(err,result)=>{
+            if(err){
+                return res.json({status:500,message:'获取信息失败'})
+            }
+            else{
+                return res.json({status:200,userInfo:result,total:result.length,page:20,pageSize:10})
+            }
+        })
+    }
+   
+})
+// 操作黑名单
+router.post('/opertionBlack',(req,res)=>{
+    const {userId,isBlack} = req.body
+    const sqlStr = `update user set isBlack='${isBlack}' where userId ='${userId}' `
+    conn.query(sqlStr,(err,results)=>{
+        if(err){
+            return res.json({status:500,message:'操作失败'})
+        }
+        else{
+            return res.json({status:200,message:'操作成功'})
+        }
+    })
+})
+// 举报用户
+router.post('/addReport',(req,res)=>{
+    const sqlStr = `select * from user where username='${req.body.username}'`
+    conn.query(sqlStr,(err,result)=>{
+        const obj =result[0]
+        obj.times =obj.times+1
+        const arr = obj.timespeople.split(',')
+        const isHave = arr.some(item=>item===req.body.timespeople)
+        if(isHave){
+            return res.json({status:200,message:'已举报，无法重复举报'})
+        }     
+        obj.timespeople = [obj.timespeople,req.body.timespeople]
+        const sql = `update user set times='${obj.times}', timespeople='${obj.timespeople}' where username='${req.body.username}'`
+        conn.query(sql,(err1,results)=>{
+            if(results.affectedRows!==0){
+                return res.json({status:200,message:'举报成功'})
+            }
+        })
+    })
+})
 
 //获取用户信息
 router.get('/userInfo',(req,res)=>{
@@ -168,15 +228,97 @@ router.get('/notcieInfo',(req,res)=>{
 })
 // 获取帖子信息
 router.get('/forumInfo',(req,res)=>{
-    const Sql = `select * from forum`
+    if(Object.keys(req.query).length){
+        const sql = `select * from forum where forumuserName='${req.query.forumuserName}'`
+        conn.query(sql,(err,result)=>{
+            if(err){
+                return res.json({status:500,message:'获取失败'})
+            }
+            else{
+                return res.json({status:200,forumList:result})
+            }
+        })
+    }
+    else{
+        const Sql = `select * from forum`
+        conn.query(Sql,(err,result)=>{
+            if(err){
+                return res.json({status:500,message:'获取失败'})
+            }
+            else{
+                return res.json({status:200,forumList:result})
+            }
+        })
+    }
+})
+
+// 修改帖子信息
+router.post('/changeForum',(req,res)=>{
+    const Sql = `update forum set ? where forumId=?`
+    conn.query(Sql,[req.body,req.body.forumId],(err,result)=>{
+  
+        if(err){
+            return res.json({status:500,message:'更新失败'})
+        }
+        else{
+            const sql = `select * from forum `
+            conn.query(sql,(errs,results)=>{
+                return res.json({status:200,message:'更新成功',forumList:results})
+            })
+        }
+    })
+})
+
+// 新建帖子信息
+router.post('/newForumInfo',(req,res)=>{
+    const forumInfo = req.body
+    const sqlStr = 'insert forum set ?'
+    conn.query(sqlStr,forumInfo,(err,results)=>{
+        if(err){
+            return res.json({status:500,message:'新建失败'})
+        }
+        else{
+            return res.json({status:200,message:'新建成功'})
+        }
+    })
+})
+
+//获取评论信息
+router.get('/commentInfo',(req,res)=>{
+    const Sql = `select * from comment where forumId='${req.query.id}'`
     conn.query(Sql,(err,result)=>{
         if(err){
             return res.json({status:500,message:'获取失败'})
         }
-        else{
-            return res.json({status:200,forumList:result})
+        else{      
+            const data = []
+            result.forEach(item => {
+                data.push(item.commentcontent)
+            });     
+            return res.json({status:200,commentsList:data})
         }
     })
 })
+// 添加评论
+router.post('/addcommentInfo',(req,res)=>{
+    const params ={ ...req.body}
+    params.commentcontent = JSON.stringify(params.commentcontent)
+    const updateRecordSql = `insert comment set ?`
+    const Sql = `select * from forum where forumId='${params.forumId}'`
+    conn.query(Sql,(err,result)=>{
+        const num = result[0].commentNum+1
+        const insertSql = `update forum set commentNum='${num}'`
+        conn.query(insertSql,(err,results)=>{
+        })
+    })
+    conn.query(updateRecordSql,params,(err,result)=>{
+        if(err){
+            return res.json({status:500,message:'添加失败'})
+        }
+        else{
+            return res.json({status:200,message:'添加成功'})
+        }
+    })
+}) 
 // 3 把router导出
 module.exports = router;
